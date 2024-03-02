9d528acb2ef7b25189c8be74bf20a1559a422fcd
jamal1608 <ahmedbhatti909@gmail.com>
2024-03-02 11:30:05 +0500

    city js

---
+++ app1.js
@@ -0,0 +1,10 @@
+var cityName = prompt("Enter the city name:");
+
+
+if (cityName.toLowerCase() === "karachi") {
+
+    alert("Welcome to the city of lights!");
+} else {
+    
+    alert("Welcome to " + cityName);
+}
